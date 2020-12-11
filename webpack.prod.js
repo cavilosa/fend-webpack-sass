abb2ff9a39d86bc009b2fe7bb83b6e01b5c079e8
calosa <cavilosa33@gmail.com>
2020-12-11 08:33:17 -0700

    npm install mini-css-extract-plugin npm install optimize-css-assets-webpack-plugin npm install terser-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,10 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -18,7 +22,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             }
         ]
     },
@@ -26,6 +30,14 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
-    ]
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
 }
